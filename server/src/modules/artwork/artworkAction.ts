4167d8100098b1deaef7eadcfe8b2c62b2cc4a63
Martin DEBOUVILLE <debouvillemartin31@gmail.com>
2025-06-11 14:19:08 +0200

    fix biome

--- server/src/modules/artwork/artworkAction.ts
+++ server/src/modules/artwork/artworkAction.ts
@@ -2,13 +2,12 @@ import artworkRepository from "./artworkRepository";
 import type { RequestHandler } from "express";
 
 const getAll: RequestHandler = async (req, res, next) => {
+  try {
+    const result = await artworkRepository.selectAll();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-    try {
-        const result = await artworkRepository.selectAll();
-        res.json(result);
-    } catch (error) {
-        next(error);
-    }
-}
-
-export default { getAll }
+export default { getAll };
