70eaed792f08cf6f7b57e46192d8a326760a2b4e
Martin DEBOUVILLE <debouvillemartin31@gmail.com>
2025-06-17 12:34:33 +0200

    fix css

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,13 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
+import App from "./App";
 
 // Create router configuration with routes
 // You can add more routes as you build out your app!
 export const router = createBrowserRouter([
   {
-    path: "/", // The root path
-    element: <Home />, // Renders the Home component for the home page
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
-
-  // Try adding a new route! For example, "/about" with an About component
 ]);
