a0bbead03043497f1e12154fb60be6d2b36de4c5
jimmy <jimmy.balay97@gmail.com>
2025-06-17 11:18:28 +0200

    composant-searchbar

---
+++ client/src/components/Searchbar.tsx
@@ -0,0 +1,12 @@
+import "./Searchbar.css"
+
+function Searchbar () {
+    <article className="DivNavBarFiltre">
+          <input type="text" />
+          <div className="divFiltre">
+            <p>picto filtre</p>
+          </div>
+        </article>
+}
+
+export default Searchbar
